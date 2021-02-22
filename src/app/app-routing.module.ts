0eb7905f4f1e8ee9b9c73527116546897be1ac21
Altaf Quadri <altafmquadri@gmail.com>
2021-02-22 16:53:35 -0500

    add lazy loading to elements and collections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { NotFoundComponent } from '../app/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'collections',
+    loadChildren: () =>
+      import('./collections/collections.module').then(m => m.CollectionsModule)
+  },
+  {
+    path: 'elements',
+    loadChildren: () =>
+      import('./elements/elements.module').then((m) => m.ElementsModule),
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ElementsHomeComponent } from './elements-home/elements-home.component'
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 const routes: Routes = [
   {
-    path: 'elements',
-    component: ElementsHomeComponent
-  }
+    path: '',
+    component: ElementsHomeComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ElementsRoutingModule { }
+export class ElementsRoutingModule {}
