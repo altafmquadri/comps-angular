3caf1cb96f24cba127b6f7a2f98dec1fcfe1e021
Altaf Quadri <altafmquadri@gmail.com>
2021-02-24 15:53:04 -0500

    customized modal

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModalComponent } from './modal/modal.component';
-
+import { SharedModule } from '../shared/shared.module';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
-  declarations: [ModsHomeComponent, ModalComponent],
-  imports: [
-    CommonModule,
-    ModsRoutingModule
-  ]
+  declarations: [ModsHomeComponent, ModalComponent, AccordionComponent],
+  imports: [CommonModule, ModsRoutingModule, SharedModule],
 })
-export class ModsModule { }
+export class ModsModule {}
--- src/app/views/item-list/item-list.component.ts
+++ src/app/views/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ɵɵNgOnChangesFeature } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
